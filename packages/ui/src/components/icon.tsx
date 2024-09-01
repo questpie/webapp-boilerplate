c4980fa65824049b4f32c3962532085ce4e9d7ac
drepkovsky <dominik.repkovsky@gmail.com>
2024-09-01 16:57:09 +0200

    fix: icon suspension

--- packages/ui/src/components/icon.tsx
+++ packages/ui/src/components/icon.tsx
@@ -1,10 +1,20 @@
 'use client'
-import { Icon as IconOg, loadIcon, type IconProps } from '@iconify/react'
+import { Icon as IconOg, loadIcon, type IconProps as IconPropsOg } from '@iconify/react'
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { Suspense, type ReactNode } from 'react'
 
-export function Icon(props: IconProps) {
+export type IconProps = IconPropsOg & {
+  /**
+   * If is true-ish wraps the component in suspense
+   * @default true
+   *
+   */
+  fallback?: boolean | ReactNode
+}
+
+function IconInternal(props: IconPropsOg) {
   const iconQuery = useSuspenseQuery({
-    queryKey: ['icon', String(props.icon)],
+    queryKey: ['icon', props.icon],
     queryFn: () => (typeof props.icon === 'string' ? loadIcon(String(props.icon)) : props.icon),
     // always cache first
     refetchOnMount: false,
@@ -14,4 +24,14 @@ export function Icon(props: IconProps) {
   })
 
   return <IconOg {...props} icon={iconQuery.data} ssr />
+}
+
+export function Icon({ fallback = true, ...props }: IconProps) {
+  if (!fallback) return <IconInternal {...props} />
+
+  return (
+    <Suspense fallback={fallback === true ? null : fallback}>
+      <IconInternal {...props} />
+    </Suspense>
+  )
 }
