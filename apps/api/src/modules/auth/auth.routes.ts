6a632bb4a5161038f873689c9ec8841c46826bb3
drepkovsky <dominik.repkovsky@gmail.com>
2024-09-01 13:31:14 +0200

    feat: add cache package

--- apps/api/src/modules/auth/auth.routes.ts
+++ apps/api/src/modules/auth/auth.routes.ts
@@ -1,6 +1,6 @@
 import { magicLinkRoutes } from '@questpie/api/modules/auth/magic-link/magic-link.routes'
 import { googleRoutes } from '@questpie/api/modules/auth/oauth/google.routes'
-import { pusherAuthRoute } from '@questpie/api/modules/auth/pusher/pusher.route'
+import { pusherAuthRoute } from '@questpie/api/modules/auth/pusher/pusher.routes'
 import { sessionRoutes } from '@questpie/api/modules/auth/session/session.routes'
 import Elysia from 'elysia'
 
